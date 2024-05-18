4e74e67a829b9dc1b174619b82311a970e71cfec
shuihon <shuhonrin@gmail.com>
2024-05-18 17:59:53 +0900

    优化递归，使用dp，暂存结果，避免重复计算，解决唯一路径问题，时间复杂度低，但空间复杂度高了

--- leetCodePractice/src/letCode/dynamicProgram/UniquePaths.java
+++ leetCodePractice/src/letCode/dynamicProgram/UniquePaths.java
@@ -5,6 +5,8 @@ package letCode.dynamicProgram;
  * on 2024/5/18.
  */
 public class UniquePaths {
+    int dp[][];
+
     public static void main(String[] args) {
         UniquePaths solution = new UniquePaths();
         int m = 3;
@@ -14,10 +16,26 @@ public class UniquePaths {
     }
 
     public int uniquePaths(int m, int n) {
+        dp = new int[m+1][n+1];
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return helper(m, n);
+    }
+
+    int helper(int m, int n) {
         if (m == 1 || n == 1) {
             return 1;
         }
-        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+        if (dp[m][n] != -1) {
+            return dp[m][n];
+        }
+
+        dp[m][n] = helper(m - 1, n) + helper(m, n - 1);
+        return dp[m][n];
+
     }
 
 
